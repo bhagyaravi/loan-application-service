72a205cf1eb868d09c457c40386800f87d02fce2
bhagyaravi <bhagyaravi98@gmail.com>
2025-04-14 16:55:57 +0530

    initial commit

---
+++ src/main/java/com/bank/loanapplication/service/LoanApplicationService.java
@@ -0,0 +1,11 @@
+package com.bank.loanapplication.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoanApplicationService {
+
+    public String processLoanApplication() {
+        return "Processing Loan Application";
+    }
+}
